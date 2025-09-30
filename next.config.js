191e4b02bf45883c7cd4d87ae25a9f989d8eadd9
meko568 <mohammedelbardan82@gmail.com>
2025-09-30 10:29:53 -0900

    updated

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true
-  },
-  // Optional: Add basePath if your site is served from a subdirectory
-  // basePath: '/portfolio',
-  // Optional: Add assetPrefix for static exports
-  // assetPrefix: './',
-}
-
-module.exports = nextConfig
+module.exports = {
+    output: 'export',
+    images: {
+      unoptimized: true
+    },
+    basePath: '/my-portfolio',
+    assetPrefix: '/my-portfolio/',
+  }
+  
