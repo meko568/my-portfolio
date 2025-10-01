a1ece201daaedfd6c903b34497218767d73866b3
meko568 <mohammedelbardan82@gmail.com>
2025-10-01 03:50:37 -0900

    updated

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: '/my-portfolio',
-  assetPrefix: '/my-portfolio/',
+  output: "export",
+  basePath: "/my-portfolio",  // 👈 your repo name
+  assetPrefix: "/my-portfolio/",
 };
 
 module.exports = nextConfig;
