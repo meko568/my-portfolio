a3d9db1ce66b27c408d1cdbe2e248f32f4bbc11d
meko568 <mohammedelbardan82@gmail.com>
2025-10-01 03:45:37 -0900

    updated

--- next.config.js
+++ next.config.js
@@ -1,15 +1,11 @@
-const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: isGithubActions ? '/my-portfolio' : '', // Replace 'my-portfolio' with your repository name
-  assetPrefix: isGithubActions ? 'https://meko568.github.io/my-portfolio/' : '', // Replace with your GitHub username and repo name
   images: {
     unoptimized: true,
   },
-  // Optional: Add trailingSlash if you want to ensure links work with GitHub Pages
-  trailingSlash: true,
+  basePath: '/my-portfolio',
+  assetPrefix: '/my-portfolio/',
 };
 
 module.exports = nextConfig;
